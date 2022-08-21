b6583e7bd7bdfa9771b6dc6c32e2e1388c50e584
Cecilia <cecilia.perdomo@gmail.com>
2022-08-20 21:53:34 -0300

    commit 1

---
+++ productos/api/models/Productos.js
@@ -0,0 +1,17 @@
+/**
+ * Productos.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: { type: 'string', required: true },
+    precio: { type: 'number', required: true },
+    foto: { type: 'string', required: true },
+  },
+
+};
+
